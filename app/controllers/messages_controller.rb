e9dedd13c37d5f039506954adb01dfe5b74b7086
Alisson <alissonhmb@gmail.com>
2023-01-08 11:11:07 -0300

    Add new and create Action views

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,9 +4,27 @@ class MessagesController < ApplicationController
   def index
   end
 
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+
+    if @message.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
 
   def set_message
     @message = Message.find(params[:id])
+  end
+
+  def message_params
+    params.require(:message).permite(:title, :description)
   end
 end
