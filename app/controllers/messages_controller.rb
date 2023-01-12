bce05ba673b03e104d415ad93483f28e16b4b854
Alisson <alissonhmb@gmail.com>
2023-01-12 09:55:44 -0300

    Add styling and authentication to controllers

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 class MessagesController < ApplicationController
+  before_action :authenticate_user!, except: %I[index show]
   before_action :set_message, only: %I[show edit update destroy]
 
   def index
